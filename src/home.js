9d618b2aa9141780c8eef48f0116ca7d6df4a1fa
Temidayo Kehinde <temidayo.kehinde.10@gmail.com>
2024-02-09 15:26:47 +0100

    added service images for mobile screens

--- src/home.js
+++ src/home.js
@@ -222,8 +222,22 @@ const Home = () => {
           </motion.div>
         </section>
         <section className="services_section" id="services">
+          <div className="mobile-service-images">
+            <div className="">
+              <img src="/IMG_4697.jpg" alt="" />
+            </div>
+            <div className="">
+              <img src="/IMG_3949.jpg" alt="" />
+            </div>
+            <div className="">
+              <img src="/IMG-0085.jpg" alt="" />
+            </div>
+            <div className="">
+              <img src="/IMG_4629.jpg" alt="" />
+            </div>
+          </div>
           <div className="title">
-            <h3>Worship With US</h3>
+            <h3>Join us in worship</h3>
             <span>
               Experience a profound encounter with the word of God that will
               forever transform your life.
